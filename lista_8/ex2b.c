e5a43d5605c919511c1f1dc7f9eba3f1e88c4f50
miguelzf <miguelfcs220410@gmail.com>
2025-08-28 11:50:42 -0300

    Create ex2b.c

---
+++ lista_8/ex2b.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#define MAX 5
+
+int a[MAX], b[MAX], c[MAX*2], i;
+
+void leitura(){
+	printf("Insira o valor de A:\n\n");
+	for(i=0;i<MAX;++i){
+		scanf("%i", &a[i]);	
+	}
+	printf("\nInsira o valor de B:\n\n");
+	for(i=0;i<MAX;++i){
+		scanf("%i", &b[i]);	
+	}
+}
+
+int juncao(int a[], int b[]){
+	for(i=0;i<MAX;++i){
+		c[i]=a[i];
+		c[i+MAX]=b[i];
+	}
+	return c;
+}
+
+void exiba(){
+	printf("\nO valor de C:\n\n");
+	for(i=0;i<MAX*2;++i){
+		printf("%i\n", c[i]);
+	}
+}
+int main(){
+	leitura();
+	juncao(a, b);
+	exiba();
+}
